a5d296bebbe2c650bcc6934d431a067510b2f99b
Mohin Patel <mohin.patel@berkeley.edu>
2018-03-10 15:49:02 -0800

    maybe works

--- algorithms/ES.py
+++ algorithms/ES.py
@@ -147,6 +147,10 @@ class ES():
         for p in range(self.config['n_populations']):
             logging.info("Population: {}\n{}".format(p+1, "="*30))
             noise_samples = np.random.randn(self.config['n_individuals'], len(self.master_params))
+            if self.MOR_flag:
+                rewards = np.zeros((self.config['n_individuals'], len(self.multiple_rewards)))
+            else:
+                rewards = np.zeros(self.config['n_individuals'])
             n_individual_target_reached = 0
             self.run_simulation(self.master_params, model, p, master=True) # Run master params for progress check, not used for training
             for i in range(self.config['n_individuals']):
