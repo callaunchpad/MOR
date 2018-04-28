5316c364e470e29ea8d5679efb6df0184033c358
whalish <jasonainc@berkeley.edu>
2018-04-27 17:01:20 -0700

    removed unnecessary demean

--- algorithms/ES.py
+++ algorithms/ES.py
@@ -164,11 +164,7 @@ class ES():
             tie_break = [(noise_samples[ind], crowding_distance(reward, pareto_front)) for ind,reward in pareto_front.items()]
             tie_break = sorted(tie_break, key = lambda x: x[1], reverse = True)
             top_mu.extend(i[0] for i in tie_break[:int(self.mu - len(top_mu))])
-            weighted_sum = sum(top_mu)*1.0/len(top_mu)
-            normalized_rewards = (weighted_sum - self.master_params)
-            if np.std(weighted_sum) != 0.0:
-                normalized_rewards = (weighted_sum - np.mean(weighted_sum)) / np.std(weighted_sum)
-            weighted_sum = normalized_rewards
+            weighted_sum = sum(top_mu)
 
         else:
             normalized_rewards = (rewards - np.mean(rewards))
